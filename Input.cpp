0fd6183e8fae4c79a10dd978fe618104f3392e75
Ayush <43788474+Ayushh10@users.noreply.github.com>
2018-10-10 20:16:06 +0530

    This file contains the input function of the program.
    This is file contains entries like flight ID, seating capacity, fare, flight company, destination.

---
+++ Input.cpp
@@ -0,0 +1,15 @@
+void flight::input()
+{
+cout<<"enter flight id"<<endl;
+cin>>fid;
+cout<<"enter seating capacity"<<endl;
+cin>>scap;
+cout<<"enter fare"<<endl;
+cin>>fare;
+cout<<"enter company of flight "<<endl;
+gets(company);
+cout<<"enter from"<<endl;
+gets(from);
+cout<<"enter to"<<endl;
+gets(to);
+}
