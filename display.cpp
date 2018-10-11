9f30936f90bdf049a28789b14dad4dd01c25eaeb
Ayush <43788474+Ayushh10@users.noreply.github.com>
2018-10-11 19:17:03 +0530

    This file has the output funtions.
    This file contains all the output functions used in the Airplane-Management program.

---
+++ display.cpp
@@ -0,0 +1,22 @@
+void flight::disp()
+{
+cout<<"flight id="<<fid<<endl;
+cout<<"seating capacity="<<scap<<endl;
+cout<<"fare="<<fare<<endl;
+cout<<"comapny="<<company<<endl;
+cout<<"from="<<from<<endl;
+cout<<"to="<<to<<endl;
+}
+int flight::getfid()
+{
+return fid;
+}
+int flight::getscap()
+{
+return scap;
+}
+float flight::getfare()
+{
+return fare;
+}
+
