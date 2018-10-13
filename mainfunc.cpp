fd8cf39c203952427c6cea37f7ce4ed5842bdde4
Ayush <43788474+Ayushh10@users.noreply.github.com>
2018-10-13 20:57:45 +0530

    This is the main funtion of the program.

---
+++ mainfunc.cpp
@@ -0,0 +1,17 @@
+void main()
+{
+clrscr();
+flight f;
+booking b ;
+int ch;
+do
+{
+cout<<"press 1 to add a flight "<<endl;
+cout<<"press 2 to display all available fight "<<endl;
+cout<<"press 3 to search a flight "<<endl;
+cout<<"press 4 to delete a flight "<<endl;
+cout<<"press 5 to book seats"<<endl;
+cout<<"press 6 to show Transactions"<<endl;
+cout<<"press 7 to exit"<<endl;
+
+cin>>ch;
